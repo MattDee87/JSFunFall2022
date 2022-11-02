54183e68276110675dfea25c97ac99c36f528218
Matt DeNero <mdenero87@gmail.com>
2022-11-02 17:50:27 -0400

    Homework

--- solutions/13-more-conditionals/02-short-circuiting.solution 2.js
+++ solutions/13-more-conditionals/02-short-circuiting.solution 2.js
@@ -12,5 +12,4 @@ let error = "Username already exists."; // When the test runs, this could also b
 
 // WRITE YOUR ANSWER BELOW THIS LINE
 
-
 let errorMessage = error || "An unexpected error occurred.";
