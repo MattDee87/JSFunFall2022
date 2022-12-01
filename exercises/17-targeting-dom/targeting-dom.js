ea50de312f4ce56644d9f18812d2eeceb3c3b6cc
Matt DeNero <mdenero87@gmail.com>
2022-11-30 19:40:19 -0500

    homework

--- exercises/17-targeting-dom/targeting-dom.js
+++ exercises/17-targeting-dom/targeting-dom.js
@@ -12,4 +12,25 @@
  */
 (function () {
   // Put your answers in here
+
+  const myid = document.querySelector("li");
+  console.log(myid);
+
+  const warning = document.querySelectorAll("bg-warning");
+  warning.forEach((warning) => {
+    console.log(warning);
+  });
+
+  const button = document.querySelector("button-target");
+  console.log(button);
+
+  const socialMediaLinks = document.querySelectorAll("social");
+  socialMediaLinks.forEach((link) => {
+    console.log(link);
+  });
+
+  const inputs = document.querySelectorAll("input");
+for (let input of inputs)
+    console.log(input);
+
 })();
